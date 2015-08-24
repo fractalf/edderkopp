e6b0ddb25837e473a639dafcac870b2ee4b32c3d
fractalf <post@alfmarius.net>
2015-08-24 12:28:08 +0200

    Fixed getPage demo

--- getPage.js
+++ getPage.js
@@ -17,25 +17,23 @@ if (url === undefined) {
 // Init Edderkopp
 var edderkopp = new Edderkopp();
 
-edderkopp.config.setPath('/nfs/home/alf/prisguide/node/config');
+//edderkopp.config.setPath('/home/user/config');
 var config = edderkopp.config.getByUrl(url);
-
-
-edderkopp.download.get(url).then(function(html) {
-    
-    // Load parser with html etc
-    log.verbose('[getPage] Parse');
-    var obj = {
-        url: url,
-        config: config,
-        html: html
-    }
-    //edderkopp.parser.load(html, config, url);
-    
-    // Parse html and get data specified in config
-    var data = edderkopp.parser.getData(obj);
-    log.info(data.web);
-}).catch(function (error) {
-    log.error(error);
-});
-
+if (config) {
+    edderkopp.download.get(url).then(function(html) {
+        
+        // Load parser with html etc
+        log.verbose('[getPage] Parse');
+        var obj = {
+            url: url,
+            config: config,
+            html: html
+        }
+        
+        // Parse html and get data specified in config
+        var data = edderkopp.parser.getData(obj);
+        log.info(data.web);
+    }).catch(function (error) {
+        log.error(error);
+    });
+}
