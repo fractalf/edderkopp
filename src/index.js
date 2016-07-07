fb9ae5546c54efda8fcf8cf6e38d750f2d2a43ea
fractalf <post@alfmarius.net>
2016-07-07 16:57:17 +0200

    Add web cache

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { logConsole, logFile } from "./log"
+import { logConsole, logFile } from './log';
 import config from './config';
-import download from "./download"
-import Parser from "./parser"
-import Crawler from "./crawler"
+import download from './download';
+import Parser from './parser';
+import Crawler from './crawler';
+import WebCache from './web-cache';
 
 global.VERSION = '1.0.0-alpha';
 global.USER_AGENT = 'Edderkopp/' + VERSION;
 
 let logger = { logConsole, logFile };
-export { logger, config, download, Parser, Crawler };
+export { logger, config, download, Parser, Crawler, WebCache };
---
+++ src/web-cache.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+export default class {
+
+    constructor(file) {
+        this.file = file || process.env.NODE_CONFIG_DIR || process.cwd() + '/web-cache.json';
+    }
+
+    get(url) {
+        if (!this.cache) {
+            try {
+                const f = fs.readFileSync(this.file);
+                this.cache = JSON.parse(f.toString());
+            } catch (err) {
+                this.cache = {};
+            }
+        }
+        return this.cache[url] ? this.cache[url] : false;
+    }
+
+    set(url, html) {
+        this.cache[url] = html;
+        fs.writeFileSync(this.file, JSON.stringify(this.cache));
+    }
+
+}
