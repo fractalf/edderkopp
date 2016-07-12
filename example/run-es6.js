f09a16eebd06dcf2133e941852efbae913695203
fractalf <post@alfmarius.net>
2016-07-12 11:45:02 +0200

    Clean up example a bit

--- example/run-es6.js
+++ example/run-es6.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+
+// NOTE! Run this with "npm run example" (se package.json) or transpile it with babel
 import fs from 'fs';
 import { log, config, Parser } from "../dist";
-
 
 const conf = config.get(__dirname + '/site.json');
 const html = fs.readFileSync(__dirname + '/site.html').toString();
---
+++ example/run.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+'use strict';
+
+var fs = require('fs');
+var config = require('../dist').config;
+var Parser = require('../dist').Parser;
+var log = require('../dist').log;
+
+var conf = config.get(__dirname + '/site.json');
+var html = fs.readFileSync(__dirname + '/site.html').toString();
+var parser = new Parser(html);
+var data = parser.getData(conf.pages.somePage);
+log.info(data);
