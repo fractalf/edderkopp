0e8a6a2f0a47c07f9aab1e720a9d050c269e059c
fractalf <post@alfmarius.net>
2016-07-12 10:50:08 +0200

    Fix log issue

--- example/run-src.js
+++ example/run-src.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import fs from 'fs';
-import { logger, config, Parser } from "../dist";
+import { log, config, Parser } from "../dist";
 
-const log = logger.logConsole;
-log.activate('debug');
 
 const conf = config.get(__dirname + '/site.json');
 const html = fs.readFileSync(__dirname + '/site.html').toString();
