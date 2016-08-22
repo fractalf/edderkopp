c8705ae7fb86067bed4ffe99b9d148ecdc6d5298
fractalf <post@alfmarius.net>
2016-08-22 15:12:43 +0200

    Rewrite config

--- dist/config.js
+++ dist/config.js
@@ -12,6 +12,14 @@ var _isInteger = require('babel-runtime/core-js/number/is-integer');
 
 var _isInteger2 = _interopRequireDefault(_isInteger);
 
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
 var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
@@ -22,178 +30,218 @@ var _url2 = _interopRequireDefault(_url);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/**
- * Also can use:
- * https://github.com/lorenwest/node-config
- */
+var Config = function () {
+    function Config() {
+        (0, _classCallCheck3.default)(this, Config);
 
-exports.default = { get: get, dir: dir };
-
-
-var _dir = process.env.NODE_CONFIG_DIR || process.cwd() + '/config';
-var _files = void 0; // cache
-
-function dir(dir) {
-    _dir = dir;
-}
-
-function get(arg) {
-    if ((0, _isInteger2.default)(arg)) {
-        // support id in filename (ex: configfile-<id>.js)
-        return _getById(arg);
-    } else if (arg.indexOf('http') !== -1) {
-        // support url (will look for the url property in all config files)
-        return _getByUrl(arg);
-    } else if (arg.indexOf('/') !== -1) {
-        // support full path of file (ex: /home/user/config.js)
-        return _parse(arg);
-    } else {
-        // support recursive search for config file in dir. return first found (ex: configfile.js)
-        return _getByFile(arg);
-    }
-}
-
-function _getById(id) {
-    _files = _files || _getFiles(_dir);
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-        for (var _iterator = (0, _getIterator3.default)(_files), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var file = _step.value;
-
-            var match = file.match(/-(\d+)\./);
-            if (match && match[1] == id) {
-                return _parse(file);
-            }
-        }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
-            }
-        }
+        this._dir = process.env.NODE_CONFIG_DIR || process.cwd() + '/config';
     }
 
-    return false;
-}
+    // Set config dir
 
-function _getByUrl(url) {
-    _files = _files || _getFiles(_dir);
-    var hostname = _url2.default.parse(url).hostname;
-    var _iteratorNormalCompletion2 = true;
-    var _didIteratorError2 = false;
-    var _iteratorError2 = undefined;
 
-    try {
-        for (var _iterator2 = (0, _getIterator3.default)(_files), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var file = _step2.value;
+    (0, _createClass3.default)(Config, [{
+        key: 'get',
 
-            var config = _parse(file);
-            if (config.url && config.url.entry && hostname == _url2.default.parse(config.url.entry).hostname) {
-                return config;
-            }
-        }
-    } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                _iterator2.return();
-            }
-        } finally {
-            if (_didIteratorError2) {
-                throw _iteratorError2;
-            }
-        }
-    }
 
-    return false;
-}
-
-function _getByFile(f) {
-    _files = _files || _getFiles(_dir);
-    var _iteratorNormalCompletion3 = true;
-    var _didIteratorError3 = false;
-    var _iteratorError3 = undefined;
-
-    try {
-        for (var _iterator3 = (0, _getIterator3.default)(_files), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-            var file = _step3.value;
-
-            if (file.indexOf(f) > -1) {
-                return _parse(file);
-            }
-        }
-    } catch (err) {
-        _didIteratorError3 = true;
-        _iteratorError3 = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion3 && _iterator3.return) {
-                _iterator3.return();
-            }
-        } finally {
-            if (_didIteratorError3) {
-                throw _iteratorError3;
-            }
-        }
-    }
-
-    return false;
-}
-
-// Recursivly find all files
-function _getFiles(dir) {
-    var files = [];
-    var _iteratorNormalCompletion4 = true;
-    var _didIteratorError4 = false;
-    var _iteratorError4 = undefined;
-
-    try {
-        for (var _iterator4 = (0, _getIterator3.default)(_fs2.default.readdirSync(dir)), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-            var file = _step4.value;
-
-            if (_fs2.default.statSync(dir + '/' + file).isDirectory()) {
-                files = files.concat(_getFiles(dir + '/' + file));
+        // Get config
+        value: function get(arg) {
+            if ((0, _isInteger2.default)(arg)) {
+                // support id in filename (ex: configfile-<id>.js)
+                return this._getById(arg);
+            } else if (arg.indexOf('http') !== -1) {
+                // support url (will look for the url property in all config files)
+                return this._getByUrl(arg);
+            } else if (arg.indexOf('/') !== -1) {
+                // support full path of file (ex: /home/user/config.js)
+                return this._parse(arg);
             } else {
-                files.push(dir + '/' + file);
+                // support recursive search for config file in dir. return first found (ex: configfile.js)
+                return this._getByFile(arg);
             }
         }
-    } catch (err) {
-        _didIteratorError4 = true;
-        _iteratorError4 = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion4 && _iterator4.return) {
-                _iterator4.return();
+
+        // Get config by id. Match id with all files found in _getFiles
+
+    }, {
+        key: '_getById',
+        value: function _getById(id) {
+            this._init();
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = (0, _getIterator3.default)(this._files), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var file = _step.value;
+
+                    var match = file.match(/-(\d+)\./);
+                    if (match && match[1] == id) {
+                        return this._parse(file);
+                    }
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
             }
-        } finally {
-            if (_didIteratorError4) {
-                throw _iteratorError4;
+
+            return false;
+        }
+
+        // Get config by filename. Match file with all files found in _getFiles
+
+    }, {
+        key: '_getByFile',
+        value: function _getByFile(file) {
+            this._init();
+            var _iteratorNormalCompletion2 = true;
+            var _didIteratorError2 = false;
+            var _iteratorError2 = undefined;
+
+            try {
+                for (var _iterator2 = (0, _getIterator3.default)(this._files), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                    var f = _step2.value;
+
+                    if (f.indexOf(file) > -1) {
+                        return this._parse(f);
+                    }
+                }
+            } catch (err) {
+                _didIteratorError2 = true;
+                _iteratorError2 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                        _iterator2.return();
+                    }
+                } finally {
+                    if (_didIteratorError2) {
+                        throw _iteratorError2;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // Get config by url. Open all files found in _getFiles and look at the url property
+
+    }, {
+        key: '_getByUrl',
+        value: function _getByUrl(url) {
+            this._init();
+            var hostname = _url2.default.parse(url).hostname;
+            var _iteratorNormalCompletion3 = true;
+            var _didIteratorError3 = false;
+            var _iteratorError3 = undefined;
+
+            try {
+                for (var _iterator3 = (0, _getIterator3.default)(this._files), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+                    var file = _step3.value;
+
+                    var _config = this._parse(file);
+                    if (_config.url && _config.url.entry && hostname == _url2.default.parse(_config.url.entry).hostname) {
+                        return _config;
+                    }
+                }
+            } catch (err) {
+                _didIteratorError3 = true;
+                _iteratorError3 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion3 && _iterator3.return) {
+                        _iterator3.return();
+                    }
+                } finally {
+                    if (_didIteratorError3) {
+                        throw _iteratorError3;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // Get filenames
+
+    }, {
+        key: '_init',
+        value: function _init() {
+            if (!this._files) {
+                this._files = this._getFiles(this._dir);
             }
         }
-    }
 
-    ;
-    return files;
-}
+        // Open and parse file
 
-function _parse(file) {
-    var match = file.match(/.*\.([^.]*)$/);
-    if (match[1] == 'json') {
-        return JSON.parse(_fs2.default.readFileSync(file).toString());
-    } else if (match[1] == 'js') {
-        return require(file);
-    }
-}
+    }, {
+        key: '_parse',
+        value: function _parse(file) {
+            var match = file.match(/.*\.([^.]*)$/);
+            if (match[1] == 'json') {
+                return JSON.parse(_fs2.default.readFileSync(file).toString());
+            } else if (match[1] == 'js') {
+                return require(file);
+            }
+        }
+
+        // Recursivly find all filenames
+
+    }, {
+        key: '_getFiles',
+        value: function _getFiles(dir) {
+            var files = [];
+            var _iteratorNormalCompletion4 = true;
+            var _didIteratorError4 = false;
+            var _iteratorError4 = undefined;
+
+            try {
+                for (var _iterator4 = (0, _getIterator3.default)(_fs2.default.readdirSync(dir)), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+                    var file = _step4.value;
+
+                    if (_fs2.default.statSync(dir + '/' + file).isDirectory()) {
+                        files = files.concat(this._getFiles(dir + '/' + file));
+                    } else {
+                        files.push(dir + '/' + file);
+                    }
+                }
+            } catch (err) {
+                _didIteratorError4 = true;
+                _iteratorError4 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion4 && _iterator4.return) {
+                        _iterator4.return();
+                    }
+                } finally {
+                    if (_didIteratorError4) {
+                        throw _iteratorError4;
+                    }
+                }
+            }
+
+            ;
+            return files;
+        }
+    }, {
+        key: 'dir',
+        set: function set(dir) {
+            this._dir = dir;
+        }
+    }]);
+    return Config;
+}();
+
+var config = new Config();
+exports.default = config;
 //# sourceMappingURL=config.js.map
--- src/config.js
+++ src/config.js
@@ -1,83 +1,97 @@
-/**
- * Also can use:
- * https://github.com/lorenwest/node-config
- */
-
 import fs from 'fs';
 import u from 'url';
 
-export default { get, dir };
+class Config {
 
-let _dir = process.env.NODE_CONFIG_DIR || process.cwd() + '/config';
-let _files; // cache
-
-function dir(dir) {
-    _dir = dir;
-}
-
-function get(arg) {
-    if (Number.isInteger(arg)) { // support id in filename (ex: configfile-<id>.js)
-        return _getById(arg);
-    } else if (arg.indexOf('http') !== -1) { // support url (will look for the url property in all config files)
-        return _getByUrl(arg);
-    } else if (arg.indexOf('/') !== -1) { // support full path of file (ex: /home/user/config.js)
-        return _parse(arg);
-    } else { // support recursive search for config file in dir. return first found (ex: configfile.js)
-        return _getByFile(arg);
+    constructor() {
+        this._dir = process.env.NODE_CONFIG_DIR || process.cwd() + '/config';
     }
-}
 
-function _getById(id) {
-    _files = _files || _getFiles(_dir);
-    for (let file of _files) {
-        let match = file.match(/-(\d+)\./);
-        if (match && match[1] == id) {
-            return _parse(file);
+    // Set config dir
+    set dir(dir) {
+        this._dir = dir;
+    }
+
+    // Get config
+    get(arg) {
+        if (Number.isInteger(arg)) { // support id in filename (ex: configfile-<id>.js)
+            return this._getById(arg);
+        } else if (arg.indexOf('http') !== -1) { // support url (will look for the url property in all config files)
+            return this._getByUrl(arg);
+        } else if (arg.indexOf('/') !== -1) { // support full path of file (ex: /home/user/config.js)
+            return this._parse(arg);
+        } else { // support recursive search for config file in dir. return first found (ex: configfile.js)
+            return this._getByFile(arg);
         }
     }
-    return false;
-}
 
-function _getByUrl(url) {
-    _files = _files || _getFiles(_dir);
-    let hostname = u.parse(url).hostname;
-    for (let file of _files) {
-        let config = _parse(file);
-        if (config.url && config.url.entry && hostname == u.parse(config.url.entry).hostname) {
-            return config;
+    // Get config by id. Match id with all files found in _getFiles
+    _getById(id) {
+        this._init();
+        for (let file of this._files) {
+            let match = file.match(/-(\d+)\./);
+            if (match && match[1] == id) {
+                return this._parse(file);
+            }
+        }
+        return false;
+    }
+
+    // Get config by filename. Match file with all files found in _getFiles
+    _getByFile(file) {
+        this._init();
+        for (let f of this._files) {
+            if (f.indexOf(file) > -1) {
+                return this._parse(f);
+            }
+        }
+        return false;
+    }
+
+    // Get config by url. Open all files found in _getFiles and look at the url property
+    _getByUrl(url) {
+        this._init();
+        let hostname = u.parse(url).hostname;
+        for (let file of this._files) {
+            let config = this._parse(file);
+            if (config.url && config.url.entry && hostname == u.parse(config.url.entry).hostname) {
+                return config;
+            }
+        }
+        return false;
+    }
+
+    // Get filenames
+    _init() {
+        if (!this._files) {
+            this._files = this._getFiles(this._dir);
         }
     }
-    return false;
-}
 
-function _getByFile(f) {
-    _files = _files || _getFiles(_dir);
-    for (let file of _files) {
-        if (file.indexOf(f) > -1) {
-            return _parse(file);
+    // Open and parse file
+    _parse(file) {
+        var match = file.match(/.*\.([^.]*)$/);
+        if (match[1] == 'json') {
+            return JSON.parse(fs.readFileSync(file).toString());
+        } else if (match[1] == 'js') {
+            return require(file);
         }
     }
-    return false;
-}
 
-// Recursivly find all files
-function _getFiles(dir) {
-    var files = [];
-    for (let file of fs.readdirSync(dir)) {
-        if (fs.statSync(dir + '/' + file).isDirectory()) {
-            files = files.concat(_getFiles(dir + '/' + file));
-        } else {
-            files.push(dir + '/' + file);
-        }
-    };
-    return files;
-}
 
-function _parse(file) {
-    var match = file.match(/.*\.([^.]*)$/);
-    if (match[1] == 'json') {
-        return JSON.parse(fs.readFileSync(file).toString());
-    } else if (match[1] == 'js') {
-        return require(file);
+    // Recursivly find all filenames
+    _getFiles(dir) {
+        let files = [];
+        for (let file of fs.readdirSync(dir)) {
+            if (fs.statSync(dir + '/' + file).isDirectory()) {
+                files = files.concat(this._getFiles(dir + '/' + file));
+            } else {
+                files.push(dir + '/' + file);
+            }
+        };
+        return files;
     }
 }
+
+const config = new Config();
+export default config;
